637295f932246f0a1e70dd73da1bcfeb773ff3a8
mbq <329673018@qq.com>
2020-07-27 19:58:41 +0800

    test

---
+++ src/Test3.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mbq
+ * Date: 2020/7/27
+ * Time: 19:54
+ */
+
+namespace mob\aaa;
+
+class Test3
+{
+    public static function test()
+    {
+        return 'test';
+    }
+}
---
+++ src/common.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mbq
+ * Date: 2020/7/27
+ * Time: 19:55
+ */
+
+echo 'common test 1';
+function test()
+{
+    return 'common function test';
+}
